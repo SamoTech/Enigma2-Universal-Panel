174ed45fed736e0a9d5313baf7d44b8a448b6354
Ossama Hashim <samo.hossam@gmail.com>
2026-09-19 07:58:49 +0300

    Make runtime paths overrideable for isolated harnesses

--- scripts/lib/common.sh
+++ scripts/lib/common.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 PANEL_VERSION="1.3.0"
-PANEL_ROOT="/usr/lib/enigma2-universal-panel"
-PANEL_ETC="/etc/enigma2-universal-panel"
-PANEL_LOG="/var/log/enigma2-universal-panel.log"
+PANEL_ROOT="${PANEL_ROOT:-/usr/lib/enigma2-universal-panel}"
+PANEL_ETC="${PANEL_ETC:-/etc/enigma2-universal-panel}"
+PANEL_LOG="${PANEL_LOG:-/var/log/enigma2-universal-panel.log}"
 
 log() { printf '%s [%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S' 2>/dev/null)" "$1" "$2" | tee -a "$PANEL_LOG" >/dev/null; }
 info() { log INFO "$1"; }
