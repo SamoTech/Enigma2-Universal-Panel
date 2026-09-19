f0cef3baecd23ff1b28c4eb6e59268ce5ebc2dd7
Ossama Hashim <samo.hossam@gmail.com>
2026-09-19 10:04:39 +0300

    Build native Enigma2 GUI plugin foundation

---
+++ Plugins/Extensions/Enigma2UniversalPanel/plugin.py
@@ -0,0 +1,19 @@
+from Plugins.Plugin import PluginDescriptor
+from .screens import Enigma2UniversalPanel
+
+PLUGIN_NAME = "Enigma2 Universal Panel"
+PLUGIN_DESCRIPTION = "Native receiver management panel"
+PLUGIN_VERSION = "0.1.0"
+
+
+def main(session, **kwargs):
+    session.open(Enigma2UniversalPanel)
+
+
+def Plugins(**kwargs):
+    return [PluginDescriptor(
+        name=PLUGIN_NAME,
+        description=PLUGIN_DESCRIPTION,
+        where=PluginDescriptor.WHERE_PLUGINMENU,
+        fnc=main,
+    )]
